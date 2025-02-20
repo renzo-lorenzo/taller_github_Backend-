db7ab2cef3b4a329538c2e9992d5fb312b82b342
renzo-lorenzo <20211532@aloe.ulima.edu.pe>
2025-02-20 15:11:57 -0500

    Correcion de posicionamiento del POST

--- src/Controllers/GastoControllers.ts
+++ src/Controllers/GastoControllers.ts
@@ -8,44 +8,41 @@ const GastoController = () => {
 
     // Operacion para listar gastos
     router.get("/", async (req : Request, resp : Response) => {
-        const gastos = await db.Gasto.findAll() // Conectamos con la base de datos
+        const gastos = await db.Gasto.findAll() //conectamos con la base de datos
         resp.json({
             msg : "",
             gastos : gastos
         })
-
-       /*
-       Endpoint de registro de Proyecto
-       Path : "/gasto"
-       Metodo : POST
-       Input :
-        {
-            nombre : "",
-            categoria : 1
-        }
-        Output:
-        {
-            msg : ""
-        }
-       */
-       router.post("/", async (req: Request, resp, Respónse) => {
-            const nuevoGasto = req.body
-
-            const gastoCreado = await db.Gasto.create({
-                fecha : nuevoGasto.fecha,
-                categoriaId : nuevoGasto.categoriaId,
-                descripcion : nuevoGasto.descripcion,
-                recurrente: nuevoGasto.recurrente ? "Sí" : "No",
-                monto: nuevoGasto.monto,
-            })
-            resp.json({
-                msg : "",
-                gasto : gastoCreado
-            })
-       })
-
-       
     })
+
+    /*
+    Endpoint de registro de Proyecto
+    Path : "/gasto"
+    Metodo : POST
+    Input :
+    {
+        nombre : "",
+        categoria : 1
+    }
+    Output:
+    {
+        msg : ""
+    }
+    */
+    router.post("/", async (req: Request, resp: Response) => {
+        const nuevoGasto = req.body;
+    
+        const gastoCreado = await db.Gasto.create({
+            fecha: nuevoGasto.fecha,
+            categoriaId: nuevoGasto.categoriaId,
+            descripcion: nuevoGasto.descripcion,
+            recurrente: nuevoGasto.recurrente,
+            monto: nuevoGasto.monto,
+        });
+    
+        resp.json({ msg: "", gasto: gastoCreado });
+    });
+
     return [path, router]
 
 }
