d21d7e783013a0c4c7d397749e58c2c9a8e586eb
pieroslva <pieroslva@hotmail.com>
2025-02-22 16:48:54 -0500

    Correcto guardado de las contraseñas encriptadas

--- src/DAO/seeders/20250213152534-data_usuarios.js
+++ src/DAO/seeders/20250213152534-data_usuarios.js
@@ -1,37 +1,41 @@
 'use strict';
 
+const bcrypt = require("bcrypt"); // Cambiamos import por require
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const saltRounds = 10; // Nivel de encriptación
+
     await queryInterface.bulkInsert('Usuario', [
       {
         nombre: "Renzo Henry",
         username: "20211532@aloe.ulima.edu.pe",
-        password: "1532",
+        password: await bcrypt.hash("1532", saltRounds), // Encriptamos la contraseña
         estado: true
       },
       {
         nombre: "Piero Silva",
         username: "20194613@aloe.ulima.edu.pe",
-        password: "4613",
+        password: await bcrypt.hash("4613", saltRounds),
         estado: true
       },
       {
         nombre: "Isaac Sisniegas",
         username: "20191931@aloe.ulima.edu.pe",
-        password: "1931",
+        password: await bcrypt.hash("1931", saltRounds),
         estado: true
       },
       {
         nombre: "Diego Morales",
         username: "20211769@aloe.ulima.edu.pe",
-        password: "1769",
+        password: await bcrypt.hash("1769", saltRounds),
         estado: true
       },
       {
         nombre: "Rodrigo Figueroa",
         username: "20202926@aloe.ulima.edu.pe",
-        password: "2926",
+        password: await bcrypt.hash("2926", saltRounds),
         estado: true
       }
     ], {});
